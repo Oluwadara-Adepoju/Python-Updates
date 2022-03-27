ceea266189df259e2776f7bdda41ffc69f3f7b2e
Oluwadara Adepoju <daraadepoju007@gmail.com>
2022-03-28 00:02:34 +0100

    first commit

---
+++ match.py
@@ -0,0 +1,15 @@
+'''python 3.10.4 introduces  Structural Pattern Matching
+this introduces the match and case  statements which can be applied in different areas
+      
+      Syntax and operation
+      
+      match subject:
+        case <pattern_1>:
+            <action_1>
+        case <pattern_2>:
+            <action_2>
+        case <pattern_3>:
+            <action_3>
+        case _:
+            <action_wildcard>
+ '''
