9dfccef76de1bf71f2d0bc1852e23fa7da4a108f
Oluwadara Adepoju <daraadepoju007@gmail.com>
2022-03-28 00:16:28 +0100

    error messages

---
+++ error.py
@@ -0,0 +1,11 @@
+'''A more precise error, previously, errore are very hard to under especially for beginners 
+Python 3.10 has revamped the way we see error in python
+ '''
+#an example
+print('Biscuits")
+''''The line of the error was indicated and a clearer view onn the type of error
+    print('Biscuits")
+          ^
+SyntaxError: unterminated string literal (detected at line 5)
+'''
+
