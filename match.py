a44f7a3f92f88981ecc6f320ea5cc9e6073b2f9c
Oluwadara Adepoju <daraadepoju007@gmail.com>
2022-03-30 00:56:44 +0100

    Match case example

--- match.py
+++ match.py
@@ -13,3 +13,23 @@ this introduces the match and case  statements which can be applied in different
         case _:
             <action_wildcard>
  '''
+ #for example
+def python_errors(error):
+    match error:
+        case 'KeyError':
+            print('The inputted key is not recognised in the dictionary')
+
+        case 'KeyboardInterrupt':
+            print('Alert! Experienced Keyboard Interrupt')
+        case 'MemoryError':
+            print('Alert This operation ran out of memory')
+        case 'NameError':
+            print('Sorry, Python does not recognize this variable name')
+
+python_errors('KeyError')
+
+python_errors('KeyboardInterrupt')
+
+python_errors('KeyboardInterrupt')
+
+python_errors( 'NameError')
