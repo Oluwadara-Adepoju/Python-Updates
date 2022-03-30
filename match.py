fffe9100181f22c45bde8766d8db1e627edee7af
Oluwadara Adepoju <daraadepoju007@gmail.com>
2022-03-30 01:32:15 +0100

    another match case example

--- match.py
+++ match.py
@@ -14,6 +14,11 @@ this introduces the match and case  statements which can be applied in different
             <action_wildcard>
  '''
  #for example
+ ## Creating a function that helps user understan python errors
+from fileinput import filename
+from os import mkdir
+
+
 def python_errors(error):
     match error:
         case 'KeyError':
@@ -25,7 +30,8 @@ def python_errors(error):
             print('Alert This operation ran out of memory')
         case 'NameError':
             print('Sorry, Python does not recognize this variable name')
-
+        case 'SyntaxError':
+            print('Python does not understand your instruction, you probably made typographical error')
 python_errors('KeyError')
 
 python_errors('KeyboardInterrupt')
@@ -33,3 +39,23 @@ python_errors('KeyboardInterrupt')
 python_errors('KeyboardInterrupt')
 
 python_errors( 'NameError')
+python_errors('SyntaxError')
+
+# Second example
+def mac_command(command: str):# you can indicate the datatype to be used
+    match command.split():
+        case ['touch',filename]: # i'm not a mac book user, so some of commands maybe inaccurate
+            print('Creating a new file')
+        case ['cp', filename,]:
+            print('copying file')
+        case ['rm',filename]:
+            print('Deleting the file')
+        case _:
+            print('unknown command')
+    
+         
+mac_command('touch error.py')
+mac_command('cp pop.py')
+mac_command('rm pop.py',)
+mac_command('t pop.py')
+
